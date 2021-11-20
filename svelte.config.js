5bf06cbc13c1991222ce7ae1469effbaf5db1e67
Ben Weber <ben@wettergifhorn.de>
2021-11-20 12:03:46 +0100

    Add static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess';
 import { resolve } from 'path';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,6 +15,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+		adapter: adapter(),
 		vite: {
 			resolve: {
 				alias: {
