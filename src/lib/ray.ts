46e11b115c4a323905d10a76de4964c6f39d041f
Ben Weber <ben@wettergifhorn.de>
2021-11-20 12:07:24 +0100

    Update file casing

---
+++ src/lib/ray.ts
@@ -0,0 +1,11 @@
+import type { Ray } from '$types/Ray';
+import type { Pt, PtIterable } from 'pts';
+import { Line } from 'pts';
+import { MAX_TRACE_LENGTH } from './const';
+
+/** Shortcut for creating a ray */
+export const createRay = (origin: Pt, angle: number): Readonly<Ray> => ({ origin, angle });
+
+/** Converts a ray to a line with start and end points */
+export const rayToPts = (ray: Ray): Readonly<PtIterable> =>
+	Line.fromAngle(ray.origin, ray.angle, MAX_TRACE_LENGTH);
